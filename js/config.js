3c951400f495a9b6d8cb500939b4b27e5f31be0c
Dayane Bernardes <dayaneberoli@gmail.com>
2025-01-24 21:53:04 -0300

    new

--- js/config.js
+++ js/config.js
@@ -1,24 +1,51 @@
 /**
+ * Chaves de conexão ccom o Firebase.
+ * ATENÇÃO! Sempre troque essas chaves pelas chaves do seu projeto.
+ **/
+ const firebaseConfig = {
+  apiKey: "AIzaSyCUb2szN690Ml-Z0DT3BL25MWDmuutI5-o",
+  authDomain: "meu-blog-estatico-6fc38.firebaseapp.com",
+  projectId: "meu-blog-estatico-6fc38",
+  storageBucket: "meu-blog-estatico-6fc38.firebasestorage.app",
+  messagingSenderId: "179954159937",
+  appId: "1:179954159937:web:5c13df3d3d7dc48a8e11c0",
+  measurementId: "G-Y52CKH2RXJ"
+};
+
+/**
  * Configuração do site;
  **/
-
  const site = {
 
-    /**
-      * Nome do site usado na tag <title>...</title> e nas interações dinâmicas
-      **/
-    nome: "Meu Blog Estático",
+  /**
+    * Nome do site usado na tag <title>...</title> e nas interações dinâmicas
+    **/
+  nome: "Nosso Estilo Blog",
 
-    /**
-     * Logotipo do site, usado na tag <header>...</title>
-     */
-    logo: "img/logo.png",
+  /**
+   * Logotipo do site, usado na tag <header>...</title>
+   */
+  logo: "img/logo.png",
 
-    /**
-     * Controla a ação ao clicar no link do usuário logado no menu principal
-     * Se `true`, exibe o perfil do usuário → perfil.html
-     * Se `false`, faz logout direto
-     **/
-    verPerfil: true,
+  /**
+   * Controla a ação ao clicar no link do usuário logado no menu principal
+   * Se `true`, exibe o perfil do usuário → perfil.html
+   * Se `false`, faz logout direto
+   **/
+  verPerfil: true,
+
+  /**
+   * Ano de lançamento do site
+   **/
+  ano: 2022,
+
+  /**
+   * Licensa do site usada no rodapé
+   **/
+  licensa: `
+  <i class="fa-regular fa-copyright fa-rotate-180 fa-fw"></i>
+  <span>Copyleft <span id="footerAno"></span> Dayane Bernardes</span>
+  `,
 
 }
+
